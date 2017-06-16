e4e9f4d0cdd9220d32ce4d2e56143be5390c270d
Jose Vicente <jvortsromero@gmail.com>
2017-06-16 16:52:00 +0200

    Added parameters to entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
-composer create-project --prefer-dist laravel/laravel /var/www/laravel
+VERSION=5.4.*
+
+while [[ $# -gt 1 ]]; do
+    param="$1"
+    case $param in
+        -c|--create)
+        CREATE=1
+        shift
+        ;;
+        -v|--version)
+        VERSION="$2"
+        shift
+        ;;
+        *)
+        #Uknown command
+        ;;
+    esac
+    shift
+done
+
+if [[ $CREATE ]]; then
+    composer create-project --prefer-dist laravel/laravel:$VERSION /var/www/laravel
+fi
 
 exec supervisord --nodaemon
